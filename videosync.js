c515bf7088661a93ff96e0eb59368367bb9735ea
Larry Wu <lw7360@gmail.com>
2014-07-02 11:42:22 -0700

    Refactored some code

--- videosync.js
+++ videosync.js
@@ -3,7 +3,6 @@
 //
 // You can check out the demo right [here](http://larrywu.com/videosync/), or
 // view the source on [Github](https://github.com/lw7360/videosync/)
-
 // Setup
 // ---
 function VideoSync(videoId, userId, roomId) {
@@ -24,8 +23,27 @@ function VideoSync(videoId, userId, roomId) {
     // Whether the connection to the channel has been established yet.
     var linkStart = false;
 
-    var startSync = function() {
-        var requested = false;
+    var lastMsg;
+
+    var pub = function (type, time) {
+        if (lastMsg !== "" + type + time) {
+
+            pubnub.publish({
+                channel: channelId,
+                message: {
+                    recipient: "",
+                    sender: userId,
+                    type: type,
+                    time: time,
+                }
+            });
+        }
+    };
+
+
+    var keepSync = function () {
+        linkStart = true;
+        var time = player.getCurrentTime();
 
         pubnub = PUBNUB.init({
             publish_key: 'demo',
@@ -33,133 +51,62 @@ function VideoSync(videoId, userId, roomId) {
             uuid: userId
         });
 
-        var onMessage = function(m) {
-            if (m.recipient === userId && m.type === "updateResponse") {
-                initTime(m.time);
-            }
-        };
-        var onPresence = function(m) {
-            if (m.occupancy <= 1) {
-                initTime(0);
-            }
-            else if (!requested) {
-                requested = true;
-                pubnub.here_now({
-                    channel: channelId,
-                    callback: function(m) {
-
-                        for (var i=0; i<m.uuids.length; i++) {
-                            var recipient = m.uuids[i];
-                            pubnub.publish({
-                                channel: channelId,
-                                message: {
-                                    recipient: recipient,
-                                    sender: userId,
-                                    type: "updateRequest"
-                                }
-                            });
-                        }
-                        setTimeout(function() {initTime(0)}, 000);
-                    }
-                });
-            }
-        };
-
         pubnub.subscribe({
             channel: channelId,
-            callback: function(m){onMessage(m)},
-            presence: function(m){onPresence(m)},
-            heartbeat: 10
-        });
-    };
-
-        var initTime = function(time) {
-            player.seekTo(time, true);
-            keepSync();
-        };
-        
-        var lastMsg;
-
-        var pub = function(type, time) {
-            if (lastMsg !== "" + type + time) {
-
-                pubnub.publish({
-                    channel: channelId,
-                    message: {
-                        recipient: "",
-                        sender: userId,
-                        type: type,
-                        time: time,
-                    }
-                });
-            }
-        };
-
-
-        var keepSync = function() {
-            linkStart = true;
-            var time = player.getCurrentTime();
-
-            pubnub.subscribe({
-                channel: channelId,
-                callback: function(m) {
-                    lastMsg = m.recipient + m.type + m.time;
-                    if ((m.recipient === userId || m.recipient === "") && m.sender !== userId) {
-                        if (m.type === "updateRequest") {
-                            var curState = player.getPlayerState();
-                            var curTime = player.getCurrentTime();
-                            pubnub.publish({
-                                channel: channelId,
-                                message: {
-                                    type: "updateResponse",
-                                    time: curTime,
-                                    recipient: m.sender
-                                }
-                            });
-                        }
-                        else if (m.type === "pause") {
-                            player.seekTo(m.time, true);
-                            time = m.time;
-                            player.pauseVideo();
-                        }
-                        else if (m.type === "play") {
-                            if (m.time !== null) {
-                                player.seekTo(m.time, true);
+            callback: function (m) {
+                lastMsg = m.recipient + m.type + m.time;
+                if ((m.recipient === userId || m.recipient === "") && m.sender !== userId) {
+                    if (m.type === "updateRequest") {
+                        var curState = player.getPlayerState();
+                        var curTime = player.getCurrentTime();
+                        pubnub.publish({
+                            channel: channelId,
+                            message: {
+                                type: "updateResponse",
+                                time: curTime,
+                                recipient: m.sender
                             }
-                            player.playVideo();
-                        }
-                        else if (m.type === "stop") {
-                            player.stopVideo();
-                        }
-                    }
-                },
-                presence: function(m) {}
-            });
-            var z = setInterval(function() {
-                var curTime = player.getCurrentTime();
-                var curState = player.getPlayerState();
-                if (Math.abs(curTime - time) > 1) {
-                    if (curState === 2) {
-                        pub("pause", curTime);
+                        });
+                    } else if (m.type === "pause") {
+                        player.seekTo(m.time, true);
+                        time = m.time;
                         player.pauseVideo();
-                    }
-                    else if (curState === 1) {
-                        player.pauseVideo();
+                    } else if (m.type === "play") {
+                        if (m.time !== null) {
+                            player.seekTo(m.time, true);
+                        }
+                        player.playVideo();
+                    } else if (m.type === "stop") {
+                        player.stopVideo();
                     }
                 }
-                time = curTime;
-            }, 500);
-        };
+            },
+            presence: function (m) {}
+        });
+        var z = setInterval(function () {
+            var curTime = player.getCurrentTime();
+            var curState = player.getPlayerState();
+            if (Math.abs(curTime - time) > 1) {
+                if (curState === 2) {
+                    pub("pause", curTime);
+                    player.pauseVideo();
+                } else if (curState === 1) {
+                    player.pauseVideo();
+                }
+            }
+            time = curTime;
+        }, 500);
+    };
 
 
     return {
-        onPlayerReady: function(event) {
+        onPlayerReady: function (event) {
             player = event.target
             event.target.playVideo();
             event.target.pauseVideo();
-            startSync();
+            keepSync();
         },
-        onPlayerStateChange: function(event) {
+        onPlayerStateChange: function (event) {
             if (linkStart) {
                 if (event.data === 2) { // pause
                     pub("pause", player.getCurrentTime());
